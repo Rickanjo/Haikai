159e53e99c6827450cee06a4f9366a8d97e62cd9
Rick Arcanjo <Ricardo.cassettari07@gmail.com>
2023-03-10 13:41:44 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.WriteLine("Haikai");
+Console.WriteLine();
+
+Console.WriteLine("Caçador de estrelas.");
+Console.WriteLine("Chorou: seu olhar voltou");
+Console.WriteLine("com tantas! Vem vê-las!");
